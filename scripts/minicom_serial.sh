78d0faaf31afadebd522fac7e8256ebe60aa1a8e
Ganesh Halthota <ganesh.rvec@gmail.com>
2026-03-24 22:30:07 +0530

    Adding captured logs

--- scripts/minicom_serial.sh
+++ scripts/minicom_serial.sh
@@ -4,4 +4,4 @@
 # sudo apt install minicom
 # sudo usermod -aG dialout $USER
 
-minicom -b 115200 -8 -D /dev/ttyACM0 -C ~/ws/meta-rpi5-secure-aosp/logs/rpi5_boot_$(date +%Y%m%d_%H%M%S).log
+minicom -b 115200 -8 -D /dev/ttyACM0 -C ~/ws/meta-rpi5-secure-aosp/logs/serial_$(date +%Y%m%d_%H%M%S).log
